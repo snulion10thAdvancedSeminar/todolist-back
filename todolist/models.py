3e3319cde7acc44ae012d7ccfe55651965983f99
pyungjae-park <pyungjae.park@ringleplus.com>
2022-07-28 16:58:33 +0900

    Initialize app settings

---
+++ todolist/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Todo(models.Model): 
+    text = models.TextField()
+    done = models.BooleanField() 
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(User, null=True, on_delete= models.CASCADE)
+
+    def update_date(self): # 나중에 수정할 때 사용
+        self.updated_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.persona_name
