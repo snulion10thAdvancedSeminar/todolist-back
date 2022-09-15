7bbea782179060305483dafa5622d18dfd702ba4
hyemmie <lhm0710@naver.com>
2022-09-16 03:23:03 +0900

    set token at response cookie

--- accounts/views.py
+++ accounts/views.py
@@ -2,15 +2,14 @@ from inspect import trace
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import generics, serializers, status, views, permissions
-from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .serializers import RegisterSerializer, LoginSerializer, LogoutSerializer
+from .serializers import SignUpSerializer, LoginSerializer, LogoutSerializer
 
 class SignUpAPIView(generics.GenericAPIView):
-    serializer_class = RegisterSerializer
+    serializer_class = SignUpSerializer
     def post(self, request):
-        user=request.data
+        user = request.data
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception = True)
         serializer.save()
@@ -18,19 +17,27 @@ class SignUpAPIView(generics.GenericAPIView):
         return JsonResponse(data, status=status.HTTP_201_CREATED)
 
 class LoginAPIView(generics.GenericAPIView):
-    serializer_class = LoginSerializer#
+    serializer_class = LoginSerializer
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        data = { "msg": "token encoded" }
+        access_token = serializer.data["tokens"]["access"]
+        refresh_token = serializer.data["tokens"]["refresh"]
+        res = JsonResponse(data, status=status.HTTP_200_OK)
+        res.set_cookie('access_token', access_token)
+        res.set_cookie('refresh_token', refresh_token)
+        return res
 
 class LogoutAPIView(generics.GenericAPIView):
     serializer_class = LogoutSerializer
     permission_classes = [permissions.IsAuthenticated]
-
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         data = { "msg": "logout success" }
-        return JsonResponse(data, status=status.HTTP_200_OK)
+        res = JsonResponse(data, status=status.HTTP_200_OK)
+        res.set_cookie('access_token', '')
+        res.set_cookie('refresh_token', '')
+        return res
